a8e4a8e348aa3dbdd8c68fc6c03fbeaf7e538510
Jonathan Jorge <desenvolvedor.jonathan@gmail.com>
2020-03-13 19:14:46 +0000

    Fix in package.json

---
+++ build/precache-manifest.ee8879b6ef135bdfd3f6559aba4c5196.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "345c959a51c1dee67d8c8e186f0d6521",
+    "url": "/index.html"
+  },
+  {
+    "revision": "904418be488ac39938af",
+    "url": "/static/css/main.79aad81f.chunk.css"
+  },
+  {
+    "revision": "3a8b104a00a9ebe42ad2",
+    "url": "/static/js/2.0198d9fa.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.0198d9fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "904418be488ac39938af",
+    "url": "/static/js/main.c98a307c.chunk.js"
+  },
+  {
+    "revision": "c6ff5418396ffe1d869e",
+    "url": "/static/js/runtime-main.38e559a0.js"
+  }
+]);
